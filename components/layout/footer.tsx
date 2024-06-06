e0d596c3b65a56aba01c6433d4985cecd9c1d455
Colin Lee <abcolin@gmail.com>
2024-06-07 00:29:52 +0800

    wip: header-sections

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,23 +1,23 @@
 import AshwarLogo from './ashwar-logo'
 import { Button } from '@/components/ui/button'
-import { TwitterLogoIcon } from '@radix-ui/react-icons'
+import Image from 'next/image'
 
 export default function Footer() {
   return (
     <div className="grid grid-cols-3 gap-x-48 px-60 py-16 bg-zinc-950 text-white">
       <div>
         <AshwarLogo />
-        <div className="w-72 text-center mt-6">
+        <div className="w-72 text-center mt-8">
           ASH WAR is a Fully On-Chain Game for all cypto users.
         </div>
       </div>
       <div className="pl-12">
-        <p className="text-2xl mb-4">Find out more</p>
+        <p className="text-2xl mb-6">Find out more</p>
         <ul className="text-base">
-          <li className="mb-2">
+          <li className="mb-4">
             <a href="">Whitepaper</a>
           </li>
-          <li className="mb-2">
+          <li className="mb-4">
             <a href="">Pitch Deck</a>
           </li>
           <li>
@@ -26,12 +26,13 @@ export default function Footer() {
         </ul>
       </div>
       <div className="text-2xl mb-4">
-        <p className="mb-2">JOIN OUR COMMUNITY</p>
-        <div className="mb-4 flex gap-x-2 py-2">
-          <TwitterLogoIcon className="h-8 w-8" />
-          <TwitterLogoIcon className="h-8 w-8" />
-          <TwitterLogoIcon className="h-8 w-8" />
-          <TwitterLogoIcon className="h-8 w-8" />
+        <p className="mb-4">JOIN OUR COMMUNITY</p>
+        <div className="mb-6 flex gap-x-4 py-2">
+          <Image src="/contact/1.png" width={50} height={50} alt='facebook' />
+          <Image src="/contact/2.png" width={50} height={50} alt='twitter' />
+          <Image src="/contact/3.png" width={50} height={50} alt='youtube' />
+          <Image src="/contact/4.png" width={50} height={50} alt='icon' />
+          <Image src="/contact/5.png" width={50} height={50} alt='icon' />
         </div>
         <Button variant="secondary">Send us your suggestions</Button>
       </div>
