6d71518d834933839e327b0e74e550af87f90c45
Colin Lee <abcolin@gmail.com>
2024-07-06 21:00:23 +0800

    wip: profile

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -5,11 +5,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-full flex flex-col px-12 pt-8 bg-[url('/profile-bg.jpg')] bg-center	bg-origin-border bg-cover">
       <NavPanel />
-      <div className="flex flex-col md:flex-row md:overflow-hidden bg-gradient-to-b	from-transparent to-zinc-950 shadow-2xl shadow-zinc-950">
-        <div className="w-full flex-none md:w-64 pl-6">
-          <UserPanel />
-        </div>
-        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+      <div className='relative px-12 -top-14'>
+        <div className="flex flex-col pt-14 md:flex-row md:overflow-hidden bg-gradient-to-b	from-transparent to-zinc-950 shadow-2xl shadow-zinc-950">
           {children}
         </div>
       </div>
--- components/profile/nav-panel.tsx
+++ components/profile/nav-panel.tsx
@@ -1,14 +1,7 @@
 export default function UserPanel() {
   return (
     <div className="flex justify-between">
-      <div className="flex space-x-4 border border-orange-300">
-        <a
-          href="#"
-          className="flex space-x-1 items-center px-6 py-2"
-          aria-current="page"
-        >
-          <span>Overview</span>
-        </a>
+      <div className="flex space-x-4 border rounded-md border-orange-200 text-orange-200 text-xl py-1 px-8 font-semibold">
         <a
           href="#"
           className="flex space-x-1 items-center px-6 py-2"
@@ -20,12 +13,6 @@ export default function UserPanel() {
           className="flex space-x-1 items-center px-6 py-2"
         >
           <span>ChronicleDOB</span>
-        </a>
-        <a
-          href="#"
-          className="flex space-x-1 items-center px-6 py-2"
-        >
-          <span>History</span>
         </a>
       </div>
     </div>
